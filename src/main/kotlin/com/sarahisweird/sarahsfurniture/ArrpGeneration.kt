08a0103f6e92e3c514cc89ed269a772bdb9d3c48
SarahIsWeird <morningcrafter0603@gmail.com>
2022-10-09 18:13:04 +0200

    Refactor ARRP pregeneration

--- src/main/kotlin/com/sarahisweird/sarahsfurniture/ArrpGeneration.kt
+++ src/main/kotlin/com/sarahisweird/sarahsfurniture/ArrpGeneration.kt
@@ -1,7 +1,9 @@
 package com.sarahisweird.sarahsfurniture
 
+import com.sarahisweird.sarahsfurniture.util.arrp.JTexturesContext
 import com.sarahisweird.sarahsfurniture.util.arrp.addBlockState
 import com.sarahisweird.sarahsfurniture.util.arrp.addModel
+import com.sarahisweird.sarahsfurniture.util.variants.Variant
 import com.sarahisweird.sarahsfurniture.util.variants.VariantUtil
 import net.devtech.arrp.api.RRPPreGenEntrypoint
 import net.devtech.arrp.api.RuntimeResourcePack
@@ -13,58 +15,76 @@ class ArrpGeneration : RRPPreGenEntrypoint {
     }
 
     override fun pregen() {
-        register(RESOURCE_PACK)
+        register()
     }
 
-    private fun register(resourcePack: RuntimeResourcePack) {
-        val armchairParentModel = "sarahsfurniture:block/armchair"
+    private fun register() {
+        VariantUtil.WOOLS.registerHorizontalFurnitureSingleTexture("armchair")
+    }
 
-        VariantUtil.WOOLS.map { wool ->
-            val blockIdentifier = Identifier(SarahsFurniture.MOD_ID, "${wool.name}_armchair")
-            val itemIdentifier = Identifier(SarahsFurniture.MOD_ID, "item/${wool.name}_armchair")
+    private fun List<Variant>.registerHorizontalFurnitureSingleTexture(blockName: String) =
+        registerHorizontalFurniture(blockName) {
+            variable("0", it.textureName)
+            particle(it.textureName)
+        }
 
-            resourcePack.addModel(blockIdentifier) {
-                parent(armchairParentModel)
+    private fun List<Variant>.registerHorizontalFurniture(blockName: String, textures: JTexturesContext.(Variant) -> Unit) =
+        forEach { variant ->
+            val variantBlockName = "${variant.name}_${blockName}"
 
-                textures {
-                    variable("0", wool.textureName)
-                    particle(wool.textureName)
-                }
-            }
+            val blockIdentifier = Identifier(SarahsFurniture.MOD_ID, variantBlockName)
+            val itemIdentifier = Identifier(SarahsFurniture.MOD_ID, "item/$variantBlockName")
+            val modelIdentifier = Identifier(SarahsFurniture.MOD_ID, "block/$variantBlockName")
 
-            resourcePack.addModel(itemIdentifier) {
-                parent(blockIdentifier)
+            registerBlockModel(blockIdentifier, modelIdentifier) { textures.invoke(this, variant) }
+            registerBlockItemRotationFix(blockIdentifier, itemIdentifier)
+            registerHorizontalFurnitureBlockstates(blockIdentifier)
+        }
 
-                display {
-                    firstPersonRightHand {
-                        rotation(0, 225 - 90, 0)
-                        translation(0, 0, 0)
-                        scale(0.40f, 0.40f, 0.40f)
-                    }
-                }
-            }
+    private fun registerBlockModel(blockIdentifier: Identifier, modelIdentifier: Identifier, textures: JTexturesContext.() -> Unit) =
+        RESOURCE_PACK.addModel(blockIdentifier) {
+            parent(modelIdentifier)
+            textures(textures)
+        }
 
-            resourcePack.addBlockState(blockIdentifier) {
-                addMultipart {
-                    addModel(blockIdentifier)
-                    whenCondition("facing", "north")
-                }
+    /**
+     * For whatever reason the held item model on the furniture blocks is rotated 90 degrees clockwise.
+     * This method registers a fix by rotating the display vale 90 degrees counterclockwise.
+     * The values were copied from minecraft:block/block.
+     */
+    private fun registerBlockItemRotationFix(blockIdentifier: Identifier, itemIdentifier: Identifier) =
+        RESOURCE_PACK.addModel(itemIdentifier) {
+            parent(blockIdentifier)
 
-                addMultipart {
-                    addModel(blockIdentifier) { y(180) }
-                    whenCondition("facing", "south")
-                }
-
-                addMultipart {
-                    addModel(blockIdentifier) { y(90) }
-                    whenCondition("facing", "east")
-                }
-
-                addMultipart {
-                    addModel(blockIdentifier) { y(270) }
-                    whenCondition("facing", "west")
+            display {
+                firstPersonRightHand {
+                    rotation(0, 225 - 90, 0)
+                    translation(0, 0, 0)
+                    scale(0.40f, 0.40f, 0.40f)
                 }
             }
         }
-    }
+
+    private fun registerHorizontalFurnitureBlockstates(identifier: Identifier) =
+        RESOURCE_PACK.addBlockState(identifier) {
+            addMultipart {
+                addModel(identifier)
+                whenCondition("facing", "north")
+            }
+
+            addMultipart {
+                addModel(identifier) { y(180) }
+                whenCondition("facing", "south")
+            }
+
+            addMultipart {
+                addModel(identifier) { y(90) }
+                whenCondition("facing", "east")
+            }
+
+            addMultipart {
+                addModel(identifier) { y(270) }
+                whenCondition("facing", "west")
+            }
+        }
 }
